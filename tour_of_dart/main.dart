409e72bfe0729aa1a3eaf81c948709012aa283a9
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-02-05 14:17:22 +0900

    tour of dartで学習をスタート！

---
+++ tour_of_dart/main.dart
@@ -0,0 +1,19 @@
+
+void main() {
+
+  var name = 'Bob';
+
+  var year = 1995;
+
+  var antennaDiameter = 3.7;
+
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+
+  var image = {
+    'tags': ['saturn'],
+    'url': '//path/to/saturn.jpg'
+  };
+
+  print(image);
+
+}
