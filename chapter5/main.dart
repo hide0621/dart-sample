fc429fc5b5b7c35162f20f25c08adf9f12a5ef21
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-02-05 13:45:32 +0900

    算術演算子の学習をした

--- chapter5/main.dart
+++ chapter5/main.dart
@@ -1,8 +1,38 @@
 
 void main() {
-  String name = "Dart";
-  print(name);
-  print("Hello, $name");
-  name = "Flutter";
-  print("Hello, $name");
+
+  int num = 0;
+
+  //足し算
+  num++;
+
+  print(num);
+
+  //引き算
+
+  num--;
+
+  print(num);
+
+  // 掛け算
+
+  num = 10;
+
+  print(num * 10);
+
+  // 割り算（小数点第一位まで出力される）
+
+  print(num / 10);
+
+  // ~/演算子で割り算（小数点以下切り捨て）
+
+  print(num ~/ 10);
+
+  // 余り
+
+  print(num % 3);
+
+
+
+
 }
