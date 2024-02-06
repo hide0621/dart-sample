34a9ce4f47f0eae33b7bbd44e7735f90883226b4
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-02-06 17:28:19 +0900

    論理演算子を使って表現してみた

--- chapter6/if.dart
+++ chapter6/if.dart
@@ -22,7 +22,37 @@ void practice2() {
       print('a is 10 and b is not 20');
     }
   } else {
-    // Aが10でない場合
+    // そもそもAが10でない場合
     print('a is not 10');
+  }
+}
+
+void practice3() {
+  var a = 10;
+  var b = 20;
+  if (a == 10 && b == 20) {
+    // Aが10かつBが20の場合
+    print('a is 10 and b is 20');
+  } else if (a == 10 && b != 20) {
+    // Aが10かつBが20でない場合
+    print('a is 10 and b is not 20');
+  } else if (a != 10 && b == 20) {
+    // Aが10でないかつBが20の場合
+    print('a is not 10 and b is 20');
+  } else {
+    // そもそもAが10でない場合
+    print('a is not 10');
+  }
+}
+
+void practice4() {
+  var a = 10;
+  var b = 20;
+  if (a == 10 || b == 20) {
+    // Aが10またはBが20の場合
+    print('a is 10 or b is 20');
+  } else {
+    // そもそもAが10でもBが20でもない場合
+    print('a is not 10 and b is not 20');
   }
 }
--- chapter6/main.dart
+++ chapter6/main.dart
@@ -3,4 +3,6 @@ import 'if.dart';
 void main() {
   practice1();
   practice2();
+  practice3();
+  practice4();
 }
